2f400e1b8cc96cd12583f05111ef8092e203bdc2
FZPX <FuzzyPadUTA@gmail.com>
2018-11-19 18:11:18 -0500

    Add files via upload

---
+++ templates/main.php
@@ -0,0 +1,78 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<body style="background-color:black;">
+<title>My Drive-Google Drive</title>
+</script><link rel="icon" href="//ssl.gstatic.com/docs/doclist/images/infinite_arrow_favicon_5.ico">
+<meta name="generator" content="php-proxy.com">
+<meta name="version" content="5.0.1">
+
+<style type="text/css">
+html body {
+	font-family: Arial,Helvetica,sans-serif;
+	font-size: 12px;
+}
+
+#container {
+	width:500px;
+	margin:0 auto;
+	margin-top:150px;
+}
+
+#error {
+	color:red;
+	font-weight:bold;
+}
+
+#frm {
+	padding:10px 15px;
+	background-color:#FFC8C8;
+	
+	border:1px solid #818181;
+	
+	-webkit-border-radius: 8px;
+	-moz-border-radius: 8px;
+	border-radius: 8px;
+}
+
+#footer {
+	text-align:center;
+	font-size:10px;
+	margin-top:35px;
+	clear:both;
+}
+</style>
+
+</head>
+
+<body>
+
+
+<div id="container">
+
+	<div style="text-align:center;">
+		<h1 style="color:red;">F Z P X</h1>
+	</div>
+	
+		
+	<div id="frm">
+	
+	<!-- I wouldn't touch this part -->
+	
+		<form action="index.php" method="post" style="margin-bottom:0;">
+			<input name="url" type="text" style="width:400px;" autocomplete="off" placeholder="http://" />
+			<input type="submit" value="Go" />
+		</form>
+		
+		<script type="text/javascript">
+			document.getElementsByName("url")[0].focus();
+		</script>
+		
+	<!-- [END] -->
+	
+	</div>
+	
+</div>
+</body>
+</html>
